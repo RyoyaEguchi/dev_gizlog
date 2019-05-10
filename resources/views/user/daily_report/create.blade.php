23e63ce066017d2480e246c07ca1cc027953ca0a
RyoyaEguchi <ryoyaeguchi1123@gmail.com>
2019-05-10 10:03:00 +0900

    リファクタの実施

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -7,42 +7,24 @@
     <form action="{{ route('daily_report.index') }}" method="post">
       @csrf
       <input class="form-control" name="user_id" type="hidden" value="{{ Auth::user()->id }}">
-      @if ($errors->has('reporting_time'))
-        <div class="form-group form-size-small has-error text-danger">
-          <input class="form-control"  name="reporting_time"  type="date" value="{{ old('reporting_time') }}">
-          <span class="help-block"></span>
+      <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error text-danger' : '' }}">
+        <input class="form-control"  name="reporting_time"  type="date" value="{{ old('reporting_time') }}">
+        <span class="help-block">
           {{ $errors->first('reporting_time') }}
-        </div>
-      @else
-        <div class="form-group form-size-small">
-          <input class="form-control"  name="reporting_time"  type="date" value="{{ old('reporting_time') }}">
-          <span class="help-block"></span>
-        </div>
-      @endif
-      @if ($errors->has('title'))
-        <div class="form-group has-error text-danger">
-          <input class="form-control" placeholder="Title" name="title" type="text" value="{{ old('title') }}">
-          <span class="help-block"></span>
+        </span>
+      </div>
+      <div class="form-group {{ $errors->has('title') ? 'has-error text-danger' : '' }}">
+        <input class="form-control" placeholder="Title" name="title" type="text" value="{{ old('title') }}">
+        <span class="help-block">
           {{ $errors->first('title') }}
-        </div>
-      @else
-        <div class="form-group">
-          <input class="form-control" placeholder="Title" name="title" type="text" value="{{ old('title') }}">
-          <span class="help-block"></span>
-        </div>
-      @endif
-      @if ($errors->has('contents'))
-        <div class="form-group has-error text-danger">
-          <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10">{{ old('contents') }}</textarea>
-          <span class="help-block"></span>
+        </span>
+      </div>
+      <div class="form-group {{ $errors->has('contents') ? 'has-error text-danger' : '' }}">
+        <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10">{{ old('contents') }}</textarea>
+        <span class="help-block">
           {{ $errors->first('contents') }}
-        </div>
-      @else
-        <div class="form-group">
-          <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10">{{ old('contents') }}</textarea>
-          <span class="help-block"></span>
-        </div>
-      @endif
+        </span>
+      </div>
       <button type="submit" class="btn btn-success pull-right">Add</button>
     </form>
   </div>
