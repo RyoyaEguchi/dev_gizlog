f0b113b0a5c37b65327a6fe7a8643f8746ac1722
RyoyaEguchi <ryoyaeguchi1123@gmail.com>
2019-05-15 15:02:43 +0900

    seeder:QuestionsTableSeederとCommentsTableSeederの実装

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->truncate();
+        DB::table('comments')->insert([
+            [
+                'user_id' => 1,
+                'question_id' => 1,
+                'comment' => 'テストコメント',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'question_id' => 2,
+                'comment' => 'テストコメント2',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        ]);
+    }
+}
---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('questions')->truncate();
+        DB::table('questions')->insert([
+            [
+                'user_id' => 1,
+                'tag_category_id' => 1,
+                'title' => 'テストタイトル',
+                'content' => 'テストコンテンツ',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'tag_category_id' => 2,
+                'title' => 'テストタイトル2',
+                'content' => 'テストコンテンツ2',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        ]);
+    }
+}
