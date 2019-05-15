872ba01f9a6f5e5f7c9cdd09ba5aa92c6b933228
RyoyaEguchi <ryoyaeguchi1123@gmail.com>
2019-05-15 14:30:39 +0900

    migrate:questionsテーブルとcommentテーブルの実装

--- database/migrations/2019_05_11_234741_create_comments_table.php
+++ database/migrations/2019_05_11_234741_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('slack_user_id');
-            $table->string('email')->unique();
-            $table->string('avatar')->nullable();
-            $table->rememberToken();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('question_id');
+            $table->text('comment');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('question_id')->references('id')->on('questions');
         });
     }
 
@@ -33,7 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
-
